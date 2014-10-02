fc58eabb518c7ddc51772fca7344852160b8bd96
BartBien <phoenixjcam@gmail.com>
2014-10-02 11:57:11 +0200

    BinarySearch main array name update.

--- src/com/phoenixjcam/search/binary/BinarySearch.java
+++ src/com/phoenixjcam/search/binary/BinarySearch.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
  * Binary search or half-interval search algorithm. <br>
  * Divide and conquer - often binary search is mention as Divide and conquer in algorithms resources. <br>
  * Finds the position of a specified input value (the search "key") within an array sorted by key value. <br>
- * <a href="http://en.wikipedia.org/wiki/Binary_search_algorithm">http://en.wikipedia.org/wiki/Binary_search_algorithm</a> <br>
+ * <a
+ * href="http://en.wikipedia.org/wiki/Binary_search_algorithm">http://en.wikipedia.org/wiki/Binary_search_algorithm</a> <br>
  * <br>
  * <br>
  * Worst case performance O(log2 n) - for 1 000 000 elements it'll be ~20 steps<br>
  * Best case performance O(1)<br>
  * More about Time complexity:<br>
- *  <br>
- * <a href="http://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions">http://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions</a>
+ * <br>
+ * <a href="http://en.wikipedia.org/wiki/Big_O_notation#Orders_of_common_functions">http://en.wikipedia.org/wiki/
+ * Big_O_notation#Orders_of_common_functions</a>
+ * 
  * @author Bart88
  *
  */
@@ -64,7 +67,8 @@ public class BinarySearch
 
 		// sort the array
 		Arrays.sort(unsortedArray);
-
+		int[] sortedArray = unsortedArray;
+		
 		Scanner in = new Scanner(System.in);
 
 		int key;
@@ -77,7 +81,7 @@ public class BinarySearch
 
 			System.out.println("typed: " + key);
 
-			if (rank(key, unsortedArray) == -1)
+			if (rank(key, sortedArray) == -1)
 				System.out.println(key);
 		}
 		while (key != -1);
